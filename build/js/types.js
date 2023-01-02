c8f674eec3c5d74876b456cc9d74c3307272d554
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-02 23:10:20 +0500

    commit

--- build/js/types.js
+++ build/js/types.js
@@ -1,1 +1,8 @@
 "use strict";
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+const a = 3;
+const b = 5;
+const result = add(a, b);
+console.log(result);
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+const add = (num1: number, num2: number) => {
+	return num1 + num2;
+};
+
+const a = 3;
+const b = 5;
+
+const result = add(a, b);
+console.log(result);

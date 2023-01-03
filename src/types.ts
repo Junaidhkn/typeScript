9da08bda14133ad01812469678f86aaf97a4b54a
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-03 23:10:14 +0500

    commit

--- src/types.ts
+++ src/types.ts
@@ -28,3 +28,16 @@ printName({ first: 'Alice', last: 'Alisson' });
 const greet = (person: string = 'Junaid') => {
 	return ` Hi there ${person}`;
 };
+
+//  Check LeapYear Function that takes in the argument and spits out the boolean
+
+const isLeapYear = (year: number): boolean => {
+	if (year % 4 == 0 && year % 100 != 0) {
+		return true;
+	} else if (year % 400 == 0) {
+		return true;
+	}
+	return false;
+};
+
+console.log(isLeapYear(2012));

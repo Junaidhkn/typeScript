c3c0d4304184856a87821a03607a082264470551
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-09 20:03:25 +0500

    array Types

--- build/js/array.js
+++ build/js/array.js
@@ -1,1 +1,10 @@
 "use strict";
+// // Empty Array Type
+const activeUsers = [];
+// //Cannot Perform array methods nor an Item can be Added ie; push()
+// activeUsers.push();
+// console.log(activeUsers);
+// // Array types
+const users = [];
+users.push('Junaid', '21323', 'true');
+// console.log(users);
---
+++ src/array.ts
@@ -0,0 +1,13 @@
+// // Empty Array Type
+const activeUsers: [] = [];
+// //Cannot Perform array methods nor an Item can be Added ie; push()
+
+// activeUsers.push();
+// console.log(activeUsers);
+
+// // Array types
+const users: string[] = [];
+
+users.push('Junaid', '21323', 'true');
+
+// console.log(users);
